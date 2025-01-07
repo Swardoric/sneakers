4f0a89108717d1982f0a9edb6954de4bc5b0aca3
Swardoric <54156243+Swardoric@users.noreply.github.com>
2025-01-07 20:24:43 +0100

    AirMax - ajout des paires en fond et de la description

--- js/navbar.js
+++ js/navbar.js
@@ -11,22 +11,25 @@ let toggleTheme = parseInt(localStorage.getItem('toggleTheme')) || 0;
 var icon = document.getElementById('theme')
 
 function setTheme() {
+
     if (toggleTheme === 1) {
         document.documentElement.style.setProperty('--navbar', '#121212CC');
         document.documentElement.style.setProperty('--text', '#FFFFFE');
         document.documentElement.style.setProperty('--background', '#121212');
         document.documentElement.style.setProperty('--btext', '#53545F');
         document.documentElement.style.setProperty('--footer', '#16161A');
-        icon.src = `${window.location.origin}/image/logo/soleil.png`;
+        icon.src = "image/logo/soleil.png";
         theme.style.transform = 'rotate(360deg)';
+
     } else {
         document.documentElement.style.setProperty('--navbar', '#F8F9FACC');
         document.documentElement.style.setProperty('--text', '#0F0E17');
         document.documentElement.style.setProperty('--background', '#F8F9FA');
         document.documentElement.style.setProperty('--btext', '#D7D8E2');
         document.documentElement.style.setProperty('--footer', '#EAEAF3');
-        icon.src = `${window.location.origin}/image/logo/lune.png`;
+        icon.src = "image/logo/lune.png";
         theme.style.transform = 'rotate(0deg)';
+
     }
 }
 
