632e9fc843e7b42f0e37b5cfff13927c53845140
Swardoric <54156243+Swardoric@users.noreply.github.com>
2025-01-08 21:18:24 +0100

    theme sombre

--- js/navbar1.js
+++ js/navbar1.js
@@ -15,7 +15,9 @@ function setTheme() {
     if (toggleTheme === 1) {
         document.documentElement.style.setProperty('--navbar', '#121212CC');
         document.documentElement.style.setProperty('--text', '#FFFFFE');
-        document.documentElement.style.setProperty('--background', '#121212');
+        document.querySelector('.background').classList.remove('light');
+        document.querySelector('.background').classList.add('dark');
+        //document.documentElement.style.setProperty('--background', '#121212');
         document.documentElement.style.setProperty('--btext', '#53545F');
         document.documentElement.style.setProperty('--footer', '#16161A');
         icon.src = "../image/logo/soleil.png";
@@ -24,7 +26,9 @@ function setTheme() {
     } else {
         document.documentElement.style.setProperty('--navbar', '#F8F9FACC');
         document.documentElement.style.setProperty('--text', '#0F0E17');
-        document.documentElement.style.setProperty('--background', '#F8F9FA');
+        document.querySelector('.background').classList.remove('dark');
+        document.querySelector('.background').classList.add('light');
+        // document.documentElement.style.setProperty('--background', '#F8F9FA');
         document.documentElement.style.setProperty('--btext', '#D7D8E2');
         document.documentElement.style.setProperty('--footer', '#EAEAF3');
         icon.src = "../image/logo/lune.png";
